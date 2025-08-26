c42c8ad5e2abb7d360dfecd525bf72c8a1921e56
yushui <3423973461@qq.com>
2025-08-26 20:24:54 +0800

    8/26

--- src/bin/string.rs
+++ src/bin/string.rs
@@ -4,7 +4,8 @@ fn main(){
     let data_s: String = data.to_string();
     s.push('h');
     //s.insert(4, 'h');
-    s.push_str(&data_s);
-    println!("{}",s);
+    //s.push_str(&data_s);
+    let s1 = format!("{}{}",s,data_s);
+    println!("{}",s1);
 
-}
+} 
