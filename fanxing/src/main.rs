02ce2ab01f9a2cf4869c219a7f4a9fbb0163cf88
yushui <3423973461@qq.com>
2025-09-02 16:05:47 +0800

    编译提交

--- fanxing/src/main.rs
+++ fanxing/src/main.rs
@@ -10,7 +10,9 @@ fn lar<T: PartialOrd>(list: &[T]) -> &T {
 
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
-
     let result = lar(&number_list);
     println!("The largest number is: {}", result);
+    let words = vec!["d","s","f","a"];
+    let result2 = lar(&words);
+    println!("The largest words is :{} ",result2);
 }
